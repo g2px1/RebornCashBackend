e9bb919faca9f829c1b708fde2fa31c13a694113
g2px1 <kir1kir1@yandex.ru>
2022-09-05 15:21:57 +0300

    part of new implementation JWT authorization

--- authorizationSpringService/src/main/java/com/client/eurekaclient/models/JWT/JWTAccess.java
+++ authorizationSpringService/src/main/java/com/client/eurekaclient/models/JWT/JWTAccess.java
@@ -16,43 +16,36 @@ import java.util.Date;
 import java.util.UUID;
 
 public class JWTAccess {
-    private String keyId;
+    private String keyId = UUID.randomUUID().toString();
     private ECKey ecKey;
     private final Curve currentCurve = Curve.P_521;
     private final JWSAlgorithm currentJWSAlgorithm = JWSAlgorithm.ES512;
     private JWSHeader jwsHeader;
-    private JWTClaimsSet jwtClaimsSet;
-    private SignedJWT signedJWT;
     private static final Logger logger = LoggerFactory.getLogger(JWTAccess.class);
 
-    public JWTAccess(String username) {
-        this.keyId = UUID.randomUUID().toString();
+    public JWTAccess() {
         try {
             this.ecKey = new ECKeyGenerator(this.currentCurve).keyID(this.keyId).generate();
         } catch (JOSEException e) {
             logger.error(e.getMessage());
         }
         jwsHeader = new JWSHeader.Builder(this.currentJWSAlgorithm).type(JOSEObjectType.JWT).keyID(this.keyId).build();
-        new JWTClaimsSet.Builder().issuer("https://reborn.cash").audience("https://reborn.cash").claim("username", username).expirationTime(Date.from(Instant.now().plusSeconds(0))).build();
-        signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
+    }
+
+    public String generateJWS(String username) {
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder().issuer("https://reborn.cash").audience("https://reborn.cash").claim("username", username).expirationTime(Date.from(Instant.now().plusSeconds(30 * 60))).build();
+        SignedJWT signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
         try {
-            this.signedJWT.sign(new ECDSASigner(ecKey));
+            signedJWT.sign(new ECDSASigner(this.ecKey));
         } catch (JOSEException e) {
             logger.error(e.getMessage());
         }
-    }
-
-    public String getJWS() {
         return signedJWT.serialize();
     }
 
-    public static boolean validateJWS(String jws) {
+    public boolean validateJWS(String jws) throws JOSEException {
         JWSVerifier verifier = new ECDSAVerifier(this.ecKey.toECPublicKey());
         return true;
-    }
-
-    public String getKeyId() {
-        return keyId;
     }
 
     public void setKeyId(String keyId) {
@@ -61,25 +54,5 @@ public class JWTAccess {
 
     public JWSHeader getJwsHeader() {
         return jwsHeader;
-    }
-
-    public void setJwsHeader(JWSHeader jwsHeader) {
-        this.jwsHeader = jwsHeader;
-    }
-
-    public JWTClaimsSet getJwtClaimsSet() {
-        return jwtClaimsSet;
-    }
-
-    public void setJwtClaimsSet(JWTClaimsSet jwtClaimsSet) {
-        this.jwtClaimsSet = jwtClaimsSet;
-    }
-
-    public SignedJWT getSignedJWT() {
-        return signedJWT;
-    }
-
-    public void setSignedJWT(SignedJWT signedJWT) {
-        this.signedJWT = signedJWT;
     }
 }
