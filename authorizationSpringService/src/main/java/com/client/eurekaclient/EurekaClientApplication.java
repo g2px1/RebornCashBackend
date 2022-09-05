796b29b67a61e098277889736d66630ce505a040
g2px1 <kir1kir1@yandex.ru>
2022-09-05 22:55:37 +0300

    add jwe implementation

--- authorizationSpringService/src/main/java/com/client/eurekaclient/EurekaClientApplication.java
+++ authorizationSpringService/src/main/java/com/client/eurekaclient/EurekaClientApplication.java
@@ -1,15 +1,24 @@
 package com.client.eurekaclient;
 
+import com.client.eurekaclient.models.JWT.JWEMicroserviceSecret;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.crypto.ECDHEncrypter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.text.ParseException;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 public class EurekaClientApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+    public static void main(String[] args) throws JOSEException, ParseException {
+        JWEMicroserviceSecret jweMicroserviceSecret = new JWEMicroserviceSecret();
+//        JWEObject jweObject = JWEObject.parse(jweMicroserviceSecret.generateJWE("123"));
+        System.out.println(jweMicroserviceSecret.generateJWE("123"));
+//        SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
