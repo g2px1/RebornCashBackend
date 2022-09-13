8addc9d04bea3b2b6af62d0bc4772f20241f36d1
g2px1 <kir1kir1@yandex.ru>
2022-09-13 22:50:20 +0300

    partly implement web3 service

--- web3Service/src/main/java/com/client/eurekaclient/services/contracts/ContractProvider.java
+++ web3Service/src/main/java/com/client/eurekaclient/services/contracts/ContractProvider.java
@@ -1,11 +1,9 @@
 package com.client.eurekaclient.services.contracts;
 
-import com.client.eurekaclient.services.gas.GasProvider;
 import org.web3j.contracts.eip20.generated.ERC20;
-import org.web3j.crypto.Credentials;
-import org.web3j.protocol.Web3j;
+import org.web3j.contracts.eip721.generated.ERC721;
 
 public interface ContractProvider {
-//    Credentials getCredentials(String privateKey);
     ERC20 getERC20Token(String contractAddress);
+    ERC721 getERC721Token(String contractAddress);
 }
--- web3Service/src/main/java/com/client/eurekaclient/services/contracts/StandardContractProvider.java
+++ web3Service/src/main/java/com/client/eurekaclient/services/contracts/StandardContractProvider.java
@@ -2,6 +2,7 @@ package com.client.eurekaclient.services.contracts;
 
 import com.client.eurekaclient.services.gas.GasProvider;
 import org.web3j.contracts.eip20.generated.ERC20;
+import org.web3j.contracts.eip721.generated.ERC721;
 import org.web3j.crypto.Credentials;
 
 public class StandardContractProvider extends GasProvider implements ContractProvider {
@@ -14,5 +15,10 @@ public class StandardContractProvider extends GasProvider implements ContractPro
     @Override
     public ERC20 getERC20Token(String contractAddress) {
         return ERC20.load(contractAddress, super.web3j, this.credentials, this);
+    }
+
+    @Override
+    public ERC721 getERC721Token(String contractAddress) {
+        return ERC721.load(contractAddress, super.web3j, this.credentials, this);
     }
 }
