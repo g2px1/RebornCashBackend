2038e7fa96fe001ff459bc09567041dce3553b43
g2px1 <kir1kir1@yandex.ru>
2022-09-19 22:26:01 +0300

    implement requests for Unit blockchain

---
+++ balanceService/src/main/java/com/client/eurekaclient/controllers/UnitBalance.java
@@ -0,0 +1,40 @@
+package com.client.eurekaclient.controllers;
+
+import com.client.eurekaclient.models.request.unit.TransferTokensRequests;
+import com.client.eurekaclient.services.unit.UnitService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/unitBalanceService/")
+public class UnitBalance {
+    @Autowired
+    private UnitService unitService;
+
+    @PostMapping("/sendTokens")
+    public JSONObject sendTokens(@RequestBody TransferTokensRequests transferTokensRequests) {return unitService.sendTokens(transferTokensRequests);}
+    @PostMapping("/sendUnits")
+    public JSONObject sendUnits(@RequestBody TransferTokensRequests transferTokensRequests) {return unitService.sendUnits(transferTokensRequests);}
+    @PostMapping("/createToken")
+    public JSONObject createToken(@RequestBody String bytecode) {
+        return unitService.createToken(bytecode);
+    }
+    @PostMapping("/findTx")
+    public JSONObject findTx(@RequestBody String hash) {
+        return unitService.findTx(hash);
+    }
+    @GetMapping("/blockHeight")
+    public JSONObject blockHeight() {
+        return unitService.getBlockHeight();
+    }
+    @GetMapping("/txPool")
+    public JSONObject txPool() {
+        return unitService.getTxPool();
+    }
+    @PostMapping("/getBalance")
+    public JSONObject getBalance(@RequestBody String address) {
+        return unitService.getBalance(address);
+    }
+}
